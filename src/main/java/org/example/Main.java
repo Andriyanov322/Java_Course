fc561a9afd01cff5e26fa4fc1ef6c016137ea2fe
Andriyanov322 <BombitGame@gmail.com>
2023-04-29 20:37:19 +0500

    Chang main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Всем привет");
+        System.out.println("Всем привет!");
     }
 }
