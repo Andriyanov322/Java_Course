4633fe4b4643c303ae3de9247bf71f663b452e29
Andriyanov322 <BombitGame@gmail.com>
2023-05-21 09:04:44 +0500

    Nine HomeWork

---
+++ src/main/java/org/example/HomeWorkLesson9/HomeWorkLesson9.java
@@ -0,0 +1,16 @@
+package org.example.HomeWorkLesson9;
+
+public class HomeWorkLesson9 {
+    static void abc() {
+        HomeWorkLesson9 h1 = new HomeWorkLesson9();
+        HomeWorkLesson9 h2 = new HomeWorkLesson9();
+    }
+
+    public static void main(String[] args) {
+        String s1 = new String("vsem privet");
+        abc();
+        abc();
+        HomeWorkLesson9 h1 = new HomeWorkLesson9();
+        abc();
+    }
+}
